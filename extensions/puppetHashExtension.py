4995b6fbf3765910431d4e11d55f72407a3fd497
peter brady <peterbrady17@outlook.com>
2023-11-26 17:09:32 -0500

    work through more erb to jinja syntax issues

--- extensions/puppetHashExtension.py
+++ extensions/puppetHashExtension.py
@@ -6,7 +6,7 @@ from logging import info, warning, debug, error
 
 class puppetHashExtension(Extension):
     # a set of names that trigger the extension.
-    tags = set(['each'])
+    tags = set(['phash'])
 
     def __init__(self, environment):
         super().__init__(environment)
@@ -17,11 +17,13 @@ class puppetHashExtension(Extension):
         lineno = next(parser.stream).lineno
         logging.info(f"line no ended up being {lineno}")
 
-        # now we parse a single expression that is used as cache key.
         args = [parser.parse_expression()]
         logging.info(f"the args parsed from expression {args}")
 
+        more_args = [parser.parse_expression()]
+        logging.info(f"the more_args parsed from expression {more_args}")
+
         body = parser.parse_statements(["name:end"], drop_needle=True)
         logging.info(f"the body parsed from statements is: {body}")
-        test = args[0]
-        return nodes.If(test, body, [], []).set_lineno(lineno)
+        
+        return nodes.For(args[0], more_args[0], body, [], [], True).set_lineno(lineno)
--- puppet.py
+++ puppet.py
@@ -7,12 +7,19 @@ import logging
 from logging import info, warning, debug, error
 from extensions import puppetIfExtension, jinjaExampleExt, puppetHashExtension
 from cachelib import SimpleCache
+import re
 
 #define dict of configurable images and their corresponding template file
 configurable_images = {'hiera': './templates/file.erb.xml', 'rhel-dev': 'Dockerfile.jinja'}
 
 #logging dict
 log_values = {'info': logging.INFO, 'warning': logging.WARN, 'debug': logging.DEBUG, 'error': logging.ERROR}
+
+class dict(dict):
+    def __init__(self, *args):
+        super().__init__(args)
+        self.each = ["one", "two",]
+        
 
 class PuppetDict():
     def __init__(self):
@@ -23,6 +30,14 @@ class PuppetDict():
         logging.info("Calling the puppet dict dig function")
         return "Some string"
 
+def each(iterable):
+    if isinstance(iterable, list):
+        return iterable
+    elif isinstance(iterable, dict):
+        return iterable.items()
+    else:
+        return []
+
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', required=True, choices=list(configurable_images.keys()), help='Desired image to configure')
@@ -31,12 +46,31 @@ def get_args():
     args = parser.parse_args()
     return args
 
+def format_hash(template_str):
+    template_arr = template_str.splitlines()
+    for idx,line in enumerate(template_arr):
+        found = line.find("each do |")
+        logging.info(f"Found: {found}")
+        if found > -1:
+            logging.info(f"Line {idx} in arr: {line}")
+            line = line.replace("<%", "<% phash")
+            line = line.replace(" do", "")
+            segments = line.split()
+            segments.insert(2, segments.pop(-2).replace("|",""))
+            line = " ".join(segments)
+            logging.info(f"Segments: {segments}")
+            template_arr[idx] = line
+
+    return "\n".join(template_arr)
+
 def format_template(filename):
     with open(filename, 'r') as file:
-        data = file.read().replace('@', '')
-        data = data.replace("<% if ", "<% pif ")
-        # data = data.replace(".each", ".items()")
-        # data = data.replace('|', '').replace(" do ", " for ")
+        data = file.read()
+    data = data.replace('@', '')
+    data = data.replace("<% if ", "<% pif ")
+    data = format_hash(data)
+    # data = data.replace(".each", ".items()")
+    # data = data.replace('|', '').replace(" do ", " for ")
     return data
 
 def main():
@@ -48,14 +82,19 @@ def main():
     env = Environment (
         loader=PackageLoader("puppet"),
         autoescape=select_autoescape(),
-        extensions=[jinjaExampleExt.FragmentCacheExtension, puppetIfExtension.puppetIfExtension, puppetHashExtension.puppetHashExtension],
+        extensions=[
+            # jinjaExampleExt.FragmentCacheExtension, 
+            puppetIfExtension.puppetIfExtension, 
+            puppetHashExtension.puppetHashExtension
+            ],
 	    block_start_string="<%",
 	    block_end_string="%>",
         variable_start_string="<%=",
         variable_end_string="%>"
     )
-    env.fragment_cache = SimpleCache()
-    env.compile_expression("do")
+    env.filters['each'] = each
+    # env.fragment_cache = SimpleCache()
+    # env.compile_expression("do")
     
 
     config_image = str(prog_args.config)
@@ -66,35 +105,30 @@ def main():
     template_str = format_template(template_file)
     logging.info(f'template looks like: \n\'{template_str}\'')
 
-    lexed = env.lex(template_str)
-    
-    for line in lexed:
-        logging.info(f'lexed line: \'{line}\'')
-
-    combined = " ".join(lexed)
-    logging.info(f"lexed combined: {combined}")
-
-    # preprocessed = env.preprocess(template_str)
-    # logging.info(f'the preprocessed template \n\'{preprocessed}\'')
     # template = env.parse(template_str)
     # logging.info(f"the template after parsing \n'{template}'")
-    # template = env.from_string(template_str)
+
+
+    #--------------------------------------------
+    template = env.from_string(template_str)
+    logging.info(f"the template after parsing \n'{template}'")
     
 
-    # logging.info("associating config image with the right config file")
-    # config_file = "config/" + prog_args.config + ".yaml"
-    # logging.debug(f'picking config file: \'{config_file}\'')
+    logging.info("associating config image with the right config file")
+    config_file = "config/" + prog_args.config + ".yaml"
+    logging.debug(f'picking config file: \'{config_file}\'')
 
 
-    # logging.info("opening the config file for reading")
-    # with open(config_file) as config_yaml:
-    #     config = yaml.safe_load(config_yaml)    
+    logging.info("opening the config file for reading")
+    with open(config_file) as config_yaml:
+        config = yaml.safe_load(config_yaml)    
     # config['pdict'] = PuppetDict()
-    # logging.debug(f'the contents of the config file: \'{config}\'')
+    # config['restrict'] = dict()
+    logging.debug(f'the contents of the config file: \'{config}\'')
 
-    # logging.info("rendering the docker file content based on the configuration values")
-    # dockerfile_contents = template.render(config)
-    # logging.debug(f'What will be written to the Dockerfile:\n\'{dockerfile_contents}\'\n')
+    logging.info("rendering the docker file content based on the configuration values")
+    dockerfile_contents = template.render(config)
+    logging.debug(f'What will be written to the Dockerfile:\n\'{dockerfile_contents}\'\n')
 
     # logging.info("saving configured template to Dockerfile")
     # output_file = prog_args.output_dir + 'Dockerfile'
